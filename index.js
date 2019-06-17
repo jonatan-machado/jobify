8ac754000f6294ebe65cd580403087aed39fd9c8
Jonatan Machado <jonatanmachado35@gmail.com>
2019-06-17 12:02:02 -0300

    adicionado a porta com a variavel env

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const sqlite = require("sqlite");
 const dbConnection = sqlite.open("banco.sqlite", { Promise });
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -144,7 +146,7 @@ const init = async () => {
 
 //INICAR O SERVIDOR NA PORTA 3000
 init();
-app.listen(3000, err => {
+app.listen(port, err => {
   if (err) {
     console.log("Não foi possivel iniciar o servidor do Jobify");
   } else {
